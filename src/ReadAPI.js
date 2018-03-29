77ecf0bd3e4e21a09c5686e395fbb5325bb0f8ed
Aqueum <martin.currie@aqueum.com>
2018-03-29 22:42:47 +0100

    feat: Rename book to post

---
+++ src/ReadAPI.js
@@ -0,0 +1,46 @@
+// modified from MyReads/src/BooksAPI.js
+const api = 'localhost:3001';
+
+// Generate a unique token for storing data on the backend server.
+let token = localStorage.token;
+if (!token)
+  token = localStorage.token = Math.random()
+    .toString(36)
+    .substr(-8);
+
+const headers = {
+  // 'Accept': 'application/json', //guessing this isn't needed
+  Authorization: token
+};
+
+export const get = postId =>
+  fetch(`${api}/posts/${postId}`, { headers })
+    .then(res => res.json())
+    .then(data => data.post);
+
+export const getAll = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data.posts);
+
+export const update = (post, shelf) =>
+  fetch(`${api}/posts/${post.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ shelf })
+  }).then(res => res.json());
+
+export const search = query =>
+  fetch(`${api}/search`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  })
+    .then(res => res.json())
+    .then(data => data.posts);
