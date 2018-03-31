23815c43e3fc1a332961120b00ee571f1e298fc6
Aqueum <martin.currie@aqueum.com>
2018-03-31 16:52:03 +0100

    chore: Remove cruft

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import * as ReadAPI from './ReadAPI';
 
@@ -110,17 +109,7 @@ class App extends Component {
     //console.log(this.state.categories);
     //console.log(this.state.posts);
     //console.log(this.state.comments);
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div />;
   }
 }
 
