7d9c2aaa06762149abeb7385415bd875e8bbdf86
Aqueum <martin.currie@aqueum.com>
2018-04-03 12:18:23 +0100

    fix: Comment out unused imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
-import App from './App';
+// import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 import { selectCategory, fetchPosts } from './actions';
 import rootReducer from './reducers';
