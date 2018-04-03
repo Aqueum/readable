0d3353e789185626e86bc9e6bef8628639ec2730
Aqueum <martin.currie@aqueum.com>
2018-04-03 11:08:07 +0100

    feat: Import actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,10 @@
 import { combineReducers } from 'redux';
+import {
+  SELECT_CATEGORY,
+  INVALIDATE_CATEGORY,
+  REQUEST_POSTS,
+  RECEIVE_POSTS
+} from '../actions';
 
 const categories = (state = {}, action) => {
   switch (action.type) {
