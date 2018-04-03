09902afc655ac3d603e4e79c5d4eaf5700505d3b
Aqueum <martin.currie@aqueum.com>
2018-04-03 10:38:12 +0100

    func: Add initial synchronous action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,30 @@
 export const GET_CATEGORIES = 'GET_CATEGORIES';
+export function getGategories() {
+  return {
+    type: GET_CATEGORIES
+  };
+}
+
+export const SELECT_CATEGORY = 'SELECT_CATEGORY';
+export function selectCategoty(category) {
+  return {
+    type: SELECT_CATEGORY,
+    category
+  };
+}
+
+export const REFRESH_CATEGORY = 'REFRESH_CATEGORY';
+export function refreshCategory(category) {
+  return {
+    type: REFRESH_CATEGORY,
+    category
+  };
+}
+
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+function requestPosts(category) {
+  return {
+    type: REQUEST_POSTS,
+    category
+  };
+}
