76f2570511d02b4188935bbd024ee585c666871e
Aqueum <martin.currie@aqueum.com>
2018-04-03 11:33:28 +0100

    feat: Add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+// modified from https://redux.js.org/advanced/async-actions
+
 import { combineReducers } from 'redux';
 import {
   SELECT_CATEGORY,
@@ -6,4 +8,59 @@ import {
   RECEIVE_POSTS
 } from '../actions';
 
-export default
+function selectedCategory(state = 'reactjs', action) {
+  switch (action.type) {
+    case SELECT_CATEGORY:
+      return action.category;
+    default:
+      return state;
+  }
+}
+
+function posts(
+  state = {
+    isFetching: false,
+    didInvalidate: false,
+    items: []
+  },
+  action
+) {
+  switch (action.type) {
+    case INVALIDATE_CATEGORY:
+      return Object.assign({}, state, { didInvalidate: true });
+    case REQUEST_POSTS:
+      return Object.assign({}, state, {
+        isFetching: true,
+        didInvalidate: false
+      });
+    case RECEIVE_POSTS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        items: action.posts,
+        lastUpdated: action.receivedAt
+      });
+    default:
+      return state;
+  }
+}
+
+function postsByCategory(state = {}, action) {
+  switch (action.type) {
+    case INVALIDATE_CATEGORY:
+    case RECEIVE_POSTS:
+    case REQUEST_POSTS:
+      return Object.assign({}, state, {
+        [action.category]: posts(state[action.category], action)
+      });
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  postsByCategory,
+  selectedCategory
+});
+
+export default rootReducer;
