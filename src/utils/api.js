5b4227350e14b74179a92c9a92dd807c9e71f8b6
Aqueum <martin.currie@aqueum.com>
2018-04-03 20:15:14 +0100

    fix: Export api & headers

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 // API stuff modified from MyReads/src/BooksAPI.js :  https://github.com/Aqueum/MyReads/blob/master/src/BooksAPI.js
-const api = 'http://localhost:3001';
+export const api = 'http://localhost:3001';
 
 // Generate a unique token for storing data on the backend server.
 let token = localStorage.token;
@@ -8,7 +8,7 @@ if (!token)
     .toString(36)
     .substr(-8);
 
-const headers = {
+export const headers = {
   Accept: 'application/json',
   'Content-Type': 'application/json',
   Authorization: token
