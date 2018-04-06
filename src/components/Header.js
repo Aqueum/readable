f0f74f33cb3e21660f22999a3c0fb027a2ec1eff
Aqueum <martin.currie@aqueum.com>
2018-04-06 09:54:21 +0100

    func: Add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import Header from './Header';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Header />
-      </div>
-    );
+    return <div>I'm a header</div>;
   }
 }
 
