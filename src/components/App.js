4d119113a6f57702e90c7c730f90c27cf6e8f3bc
Aqueum <martin.currie@aqueum.com>
2018-04-07 10:22:07 +0100

    feat: Add react routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import Header from '../containers/Header';
+import { Switch, Route } from 'react-router-dom';
+import FrontPage from './FrontPage';
 import List from '../containers/List';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <List />
+        <Switch>
+          <Route exact path="/" render={() => <FrontPage />} />
+          <Route exact path="/list" render={() => <List />} />
+        </Switch>
       </div>
     );
   }
