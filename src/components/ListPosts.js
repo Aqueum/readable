e8b67f0ac99c63087d10fea18123828918c823f3
Aqueum <martin.currie@aqueum.com>
2018-04-07 21:13:39 +0100

    feat: Add internal list sorting

--- src/components/ListPosts.js
+++ src/components/ListPosts.js
@@ -5,14 +5,25 @@ import { votePost } from '../actions/post.js';
 
 export default class ListPosts extends Component {
   render() {
+    console.log(this.props.posts);
+    const sortby = 'score';
+    const sortedPosts = [] // inspired by https://stackoverflow.com/questions/43572436/sort-an-array-of-objects-in-react-and-render-them/43572944
+      .concat(this.props.posts)
+      .sort(
+        (a, b) =>
+          sortby === 'score'
+            ? a.voteScore < b.voteScore
+            : a.timestamp < b.timestamp
+      );
     return (
       <div>
         <ul>
-          {this.props.posts.map(post => (
+          {sortedPosts.map(post => (
             <li key={post.title}>
               <Link to={post.category + '/' + post.id}>{post.title}</Link> by{' '}
               {post.author}
-              , {post.commentCount} comments, score = {post.voteScore}
+              , {post.commentCount} comments, score = {post.voteScore}, time ={' '}
+              {post.timestamp}
               <button onClick={votePost(post.id, 'upVote')}>+</button>
               <button onClick={votePost(post.id, 'downVote')}>-</button>
               <button onClick={null}>edit</button>
