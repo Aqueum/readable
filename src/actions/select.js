2f32629c2ab9488772f469635f41c973e0de65f0
Aqueum <martin.currie@aqueum.com>
2018-04-11 10:26:33 +0100

    fix: Change action input to state variable

--- src/actions/select.js
+++ src/actions/select.js
@@ -2,7 +2,7 @@ export const SELECT_CATEGORY = 'SELECT_CATEGORY';
 export function selectCategory(category) {
   return {
     type: SELECT_CATEGORY,
-    category
+    selectCat: category
   };
 }
 
@@ -10,6 +10,6 @@ export const SELECT_SORT = 'SELECT_SORT';
 export function selectSort(sortBy) {
   return {
     type: SELECT_SORT,
-    sortBy
+    selectSort: sortBy
   };
 }
