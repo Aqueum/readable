8f4e6f2b0758b1ea33cd02c694355c9b9bd6ee7b
Aqueum <martin.currie@aqueum.com>
2018-04-11 12:59:30 +0100

    feat:  Add new post form

--- src/containers/NewPost.js
+++ src/containers/NewPost.js
@@ -1,9 +1,82 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { addPost } from '../actions/post';
 
-class NewPost extends Component {
-  render() {
-    return <div>New Post Page - coming soon</div>;
-  }
-}
+const NewPost = ({ dispatch }) => {
+  let id, title, body, author;
 
-export default NewPost;
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (!title.value.trim()) {
+            return;
+          }
+          dispatch(
+            addPost(id.value, title.value, body.value, author.value, 'udacity')
+          );
+        }}
+      >
+        <p>
+          Post ID <input name="id" ref={node => (id = node)} />
+        </p>
+        <p>
+          Post Title <input name="title" ref={node => (title = node)} />
+        </p>
+        <p>
+          Post Author <input name="author" ref={node => (author = node)} />
+        </p>
+        <p>
+          Post Body <textarea ref={node => (body = node)} />
+        </p>
+        <button type="submit">Add Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default connect()(NewPost);
+
+/*const NewPost = ({ dispatch }) => {
+  let id, timestamp, title, body, author, category; // was input;
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (!title.value.trim()) {
+            return;
+          }
+          dispatch(
+            addPost(
+              id.value,
+              timestamp.value,
+              title.value,
+              body.value,
+              author.value,
+              category.value
+            )
+          );
+          //input.value = '';
+          id.value = '';
+          timestamp.value = '';
+          title.value = '';
+          body.value = '';
+          author.value = '';
+          category.value = '';
+        }}
+      >
+        <input ref={node => (input = node)} />
+        <button type="submit">Add Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default connect()(NewPost);*/
+
+/*
+addPost(id, timestamp, title, body, author, category)
+*/
