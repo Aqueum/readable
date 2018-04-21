7329d0559a24ba1220afc657af1ee37fc3022339
Aqueum <martin.currie@aqueum.com>
2018-04-21 17:29:55 +0100

    feat: Comment line display

---
+++ src/components/Comment/ShowCommentLine.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import ButtonBar from '../../containers/ButtonBar';
+import timeConverter from '../../utils/timeconverter';
+
+export default class ShowPostLine extends Component {
+  render() {
+    const { post } = this.props;
+    return (
+      <div>
+        {comment.title} by {comment.author} on{' '}
+        {timeConverter(comment.timestamp)}
+        , score = {comment.voteScore}
+        <ButtonBar post={comment} />
+      </div>
+    );
+  }
+}
+
+ShowPostLine.propTypes = {
+  comment: PropTypes.object.isRequired
+};
+
+/*
+<Link to={post.category + '/' + post.id}>{post.title}</Link>
+*/
