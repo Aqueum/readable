3d485f0033b22ad57cfa8ce7b2625a568fb55b8a
Aqueum <martin.currie@aqueum.com>
2018-04-21 22:31:40 +0100

    feat: Add comments to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { categories } from './categories';
 import { posts } from './posts';
 import { selections } from './selections';
+import { comments } from './comments';
 
 const rootReducer = combineReducers({
   categories,
   selections,
-  posts
+  posts,
+  comments
 });
 
 export default rootReducer;
