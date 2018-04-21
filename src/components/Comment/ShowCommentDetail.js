89d6b7e33699a1727696e1176ecc1dd7b094b43b
Aqueum <martin.currie@aqueum.com>
2018-04-21 23:28:42 +0100

    feat: Add comment view

---
+++ src/components/Comment/ShowCommentDetail.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ButtonBar from '../../containers/ButtonBar';
+
+export default class ShowCommentDetail extends Component {
+  render() {
+    const { comment } = this.props;
+    return (
+      <div>
+        <ul>
+          <p> {comment.body} </p>
+          <p>by {comment.author}</p>
+          <p>score = {comment.voteScore}</p>
+          <ButtonBar type="comment" item={comment} />
+        </ul>
+      </div>
+    );
+  }
+}
+
+ShowCommentDetail.propTypes = {
+  comment: PropTypes.object.isRequired
+};
---
+++ src/containers/Comment.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Header from '../containers/Header';
+import ShowCommentDetail from '../components/Comment/ShowCommentDetail';
+import { getComment } from '../actions/comment';
+
+class Comment extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(getComment(this.props.match.params.commentid));
+  }
+
+  render() {
+    const { comment } = this.props;
+    return (
+      <div>
+        {comment.id ? (
+          <div>
+            <Header cat="" />
+            <ShowCommentDetail comment={comment} />
+          </div>
+        ) : (
+          <div>
+            <Header cat="" />
+            <h1>404: Page Not Found</h1>
+            <p>
+              {' '}
+              a comment with id: {this.props.match.params.commentid} can't be
+              found{' '}
+            </p>
+            <a href="http://www.aqueum.com/contact/">
+              let us know there's a problem
+            </a>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+Comment.propTypes = {
+  comment: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired
+};
+
+// inspired by https://stackoverflow.com/questions/34840994/javascript-redux-how-to-get-an-element-from-store-by-id
+const mapStateToProps = (state, ownProps) => ({
+  comment: state.posts.item || {}
+});
+
+export default connect(mapStateToProps)(Comment);
