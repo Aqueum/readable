3768287bf133f32b02bf9e1e67bede5e92f1e11a
Aqueum <martin.currie@aqueum.com>
2018-04-23 21:59:51 +0100

    feat: Add delete icon

--- src/containers/ButtonBar.js
+++ src/containers/ButtonBar.js
@@ -5,15 +5,16 @@ import PropTypes from 'prop-types';
 import { votePost, delPost } from '../actions/post.js';
 import { voteComment, delComment } from '../actions/comment.js';
 import Icon from 'react-icons-kit';
-import { smile, sad } from 'react-icons-kit/icomoon';
+import { smile, sad, pencil, bin } from 'react-icons-kit/icomoon';
 
 class ButtonBar extends Component {
   render() {
     const { type, item, dispatch, score } = this.props;
     return (
-      <div className="buttonBar">
+      <div>
         <Icon
           icon={smile}
+          alt="upVote"
           onClick={() => {
             type === 'post'
               ? dispatch(votePost(item.id, 'upVote'))
@@ -23,31 +24,34 @@ class ButtonBar extends Component {
         {score}
         <Icon
           icon={sad}
+          alt="downVote"
           onClick={() => {
             type === 'post'
               ? dispatch(votePost(item.id, 'downVote'))
               : dispatch(voteComment(item.id, 'downVote'));
           }}
-        />
+        />{' '}
         {type === 'post' ? (
-          <Link to={'/' + item.category + '/' + item.id + '/edit'}>
-            <button className="button">edit</button>
+          <Link
+            className="blacklink"
+            to={'/' + item.category + '/' + item.id + '/edit'}
+          >
+            <Icon icon={pencil} alt="edit" />
           </Link>
         ) : (
-          <Link to={'/comment/' + item.id + '/edit'}>
-            <button className="button">edit</button>
+          <Link className="blacklink" to={'/comment/' + item.id + '/edit'}>
+            <Icon icon={pencil} alt="edit" />
           </Link>
-        )}
-        <button
-          className="button"
+        )}{' '}
+        <Icon
+          icon={bin}
+          alt="delete"
           onClick={() => {
             type === 'post'
               ? dispatch(delPost(item.id))
               : dispatch(delComment(item.id));
           }}
-        >
-          delete
-        </button>
+        />
         <p />
       </div>
     );
