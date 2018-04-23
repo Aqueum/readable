c390bf1d14bc3cefc89fcbfcac519668e8dff8ea
Aqueum <martin.currie@aqueum.com>
2018-04-23 10:18:43 +0100

    feat: Create new comment form calling addComment

--- src/containers/NewComment.js
+++ src/containers/NewComment.js
@@ -1,45 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addPost } from '../actions/post';
+import PropTypes from 'prop-types';
+import { addComment } from '../actions/comment';
 
-class NewPost extends Component {
+class NewComment extends Component {
   render() {
-    let title, body, author, category;
+    let body, author;
+    let parentId = this.props.match.params.postid;
 
     return (
       <div>
+        <p>New comment on post {parentId}</p>
         <form
           onSubmit={e => {
             e.preventDefault();
-            if (!title.value.trim()) {
+            if (!body.value.trim()) {
               return;
             }
             this.props.dispatch(
-              addPost(title.value, body.value, author.value, category.value)
+              addComment(body.value, author.value, parentId.value)
             );
             this.props.history.push('/');
           }}
         >
           <p>
-            Post Title <input name="title" ref={node => (title = node)} />
+            Comment Author <input name="author" ref={node => (author = node)} />
           </p>
           <p>
-            Post Author <input name="author" ref={node => (author = node)} />
+            Comment Body <textarea ref={node => (body = node)} />
           </p>
-          <p>
-            Post Body <textarea ref={node => (body = node)} />
-          </p>
-          <label>
-            Category:
-            <select defaultValue="udacity" ref={node => (category = node)}>
-              <option value="react">react</option>
-              <option value="redux">redux</option>
-              <option value="udacity">udacity</option>
-            </select>
-          </label>
           <div>
             <button className="button" type="submit">
-              Add Post
+              Add Comment
             </button>
           </div>
         </form>
@@ -48,47 +40,8 @@ class NewPost extends Component {
   }
 }
 
-export default connect()(NewPost);
-
-/*const NewPost = ({ dispatch }) => {
-  let id, timestamp, title, body, author, category; // was input;
-
-  return (
-    <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          if (!title.value.trim()) {
-            return;
-          }
-          dispatch(
-            addPost(
-              id.value,
-              timestamp.value,
-              title.value,
-              body.value,
-              author.value,
-              category.value
-            )
-          );
-          //input.value = '';
-          id.value = '';
-          timestamp.value = '';
-          title.value = '';
-          body.value = '';
-          author.value = '';
-          category.value = '';
-        }}
-      >
-        <input ref={node => (input = node)} />
-        <button type="submit">Add Post</button>
-      </form>
-    </div>
-  );
+NewComment.propTypes = {
+  match: PropTypes.object.isRequired
 };
 
-export default connect()(NewPost);*/
-
-/*
-addPost(id, timestamp, title, body, author, category)
-*/
+export default connect()(NewComment);
